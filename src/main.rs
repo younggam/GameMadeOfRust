af367d70f4993bb27ba5d11ea72091e007799f17
younggam <nainjoo2001@naver.com>
2022-10-23 16:49:11 +0900

    bevy init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .run();
+    App::new().add_system(hello_world).run();
+}
+
+fn hello_world() {
+    println!("hello world!");
 }
