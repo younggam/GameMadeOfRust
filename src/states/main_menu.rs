cd8bfaaea8016d432db4475966d3641f90d8da7f
younggam <nainjoo2001@naver.com>
2022-10-27 23:05:24 +0900

    Button Action fn container

--- src/states/main_menu.rs
+++ src/states/main_menu.rs
@@ -2,6 +2,16 @@ use crate::states::*;
 
 use bevy::prelude::*;
 
+const FONT_DIR: &str = "fonts/Schluber.otf";
+
+const PLAY_TEXT: &str = "Play";
+const EXIT_TEXT: &str = "Exit";
+
+const TEXT_COLOR: Color = Color::YELLOW;
+
+const BUTTON_COLOR_NONE: Color = Color::BLACK;
+const BUTTON_COLOR_HOVER: Color = Color::GRAY;
+
 pub(crate) struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
@@ -12,35 +22,80 @@ impl Plugin for MainMenuPlugin {
         )
         .add_system_set_to_stage(
             CoreStage::Update,
-            SystemSet::on_update(UpdateStageState::MainMenu).with_system(button_system),
+            SystemSet::on_update(UpdateStageState::MainMenu).with_system(interact_buttons),
         );
     }
 }
 
-fn button_system(
+#[derive(Component)]
+pub(crate) struct Action<F>(F);
+
+fn interact_buttons(
     mut interaction_query: Query<
-        (&Interaction, &mut UiColor, &Children),
+        (
+            &Interaction,
+            &mut UiColor,
+            &Action<for<'a> fn(&'a mut AppState)>,
+        ),
         (Changed<Interaction>, With<Button>),
     >,
-    mut text_query: Query<&mut Text>,
+    mut state: ResMut<AppState>,
 ) {
-    for (interaction, mut color, children) in &mut interaction_query {
-        let mut text = text_query.get_mut(children[0]).unwrap();
+    for (interaction, mut color, func) in interaction_query.iter_mut() {
         match *interaction {
             Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
-                *color = Color::YELLOW_GREEN.into();
+                let a = &mut *state;
+                func.0(a)
             }
             Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
-                *color = Color::YELLOW.into();
+                *color = BUTTON_COLOR_HOVER.into();
             }
             Interaction::None => {
-                text.sections[0].value = "Button".to_string();
-                *color = Color::WHITE.into();
+                *color = BUTTON_COLOR_NONE.into();
             }
         }
     }
+}
+
+fn button() -> ButtonBundle {
+    ButtonBundle {
+        style: Style {
+            size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+            // center button
+            margin: UiRect::all(Val::Auto),
+            // horizontally center child text
+            justify_content: JustifyContent::Center,
+            // vertically center child text
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        color: BUTTON_COLOR_NONE.into(),
+        ..default()
+    }
+}
+
+fn text(text: impl Into<String>, asset_server: &AssetServer) -> TextBundle {
+    TextBundle::from_section(
+        text,
+        TextStyle {
+            font: asset_server.load(FONT_DIR),
+            font_size: 30.0,
+            color: TEXT_COLOR,
+        },
+    )
+    .with_style(Style {
+        // center button
+        margin: UiRect {
+            top: Val::Px(7.5),
+            ..default()
+        },
+        ..default()
+    })
+}
+
+fn but(a: &mut AppState) {
+    *a = AppState::InGame;
+    *a = AppState::InGame;
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -48,76 +103,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn_bundle(Camera2dBundle::default())
         .insert(StateComponent(AppState::MainMenu));
+
     commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                // center button
-                margin: UiRect::all(Val::Auto),
-                // horizontally center child text
-                justify_content: JustifyContent::Center,
-                // vertically center child text
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            color: Color::WHITE.into(),
-            ..default()
-        })
+        .spawn_bundle(button())
         .insert(StateComponent(AppState::MainMenu))
+        .insert(Action::<for<'a> fn(&'a mut AppState)>(but))
         .with_children(|parent| {
-            parent.spawn_bundle(
-                TextBundle::from_section(
-                    "Button",
-                    TextStyle {
-                        font: asset_server.load("fonts/Schluber.otf"),
-                        font_size: 30.0,
-                        color: Color::BLACK,
-                    },
-                )
-                .with_style(Style {
-                    // center button
-                    margin: UiRect {
-                        top: Val::Px(7.5),
-                        ..default()
-                    },
-                    ..default()
-                }),
-            );
+            parent.spawn_bundle(text(PLAY_TEXT, &asset_server));
         });
+
     commands
-        .spawn_bundle(ButtonBundle {
-            style: Style {
-                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
-                // center button
-                margin: UiRect::all(Val::Auto),
-                // horizontally center child text
-                justify_content: JustifyContent::Center,
-                // vertically center child text
-                align_items: AlignItems::Center,
-                ..default()
-            },
-            color: Color::WHITE.into(),
-            ..default()
-        })
+        .spawn_bundle(button())
         .insert(StateComponent(AppState::MainMenu))
         .with_children(|parent| {
-            parent.spawn_bundle(
-                TextBundle::from_section(
-                    "Button",
-                    TextStyle {
-                        font: asset_server.load("fonts/Schluber.otf"),
-                        font_size: 30.0,
-                        color: Color::BLACK,
-                    },
-                )
-                .with_style(Style {
-                    // center button
-                    margin: UiRect {
-                        top: Val::Px(7.5),
-                        ..default()
-                    },
-                    ..default()
-                }),
-            );
+            parent.spawn_bundle(text(EXIT_TEXT, &asset_server));
         });
 }
