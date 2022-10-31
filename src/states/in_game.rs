8c90414c524285644fdf1343e79d9231f3284e15
younggam <nainjoo2001@naver.com>
2022-10-31 16:18:02 +0900

    new plane!

--- src/states/in_game.rs
+++ src/states/in_game.rs
@@ -13,8 +13,33 @@ impl Plugin for InGamePlugin {
     }
 }
 
-fn setup(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     commands
-        .spawn_bundle(Camera3dBundle::default())
+        .spawn_bundle(Camera3dBundle {
+            transform: Transform::from_xyz(-4.0, 100.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        })
         .insert(StateMark::new(AppState::InGame));
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // plane
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {
+            size: 100.0,
+        })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
 }
