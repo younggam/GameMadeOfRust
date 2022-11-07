3f1feb9028997c9627450fcf1b997660400d2826
younggam <nainjoo2001@naver.com>
2022-11-07 22:57:36 +0900

    octree

--- src/states/in_game.rs
+++ src/states/in_game.rs
@@ -1,14 +1,15 @@
 use crate::{consts::*, states::*, ui::*, Textures};
+use std::cmp::Ordering;
+use std::collections::{BTreeMap, BTreeSet};
+use std::ops::{Add, Deref, Sub};
 
+use bevy::math::DVec3;
 use bevy::{
     input::mouse::MouseMotion,
-    prelude::{shape::*, *},
+    prelude::{shape::Plane, *},
 };
 
 use bevy_polyline::prelude::*;
-
-#[derive(Component)]
-pub struct Collider;
 
 pub struct InGamePlugin;
 
@@ -138,6 +139,11 @@ fn setup(
             ..default()
         })
         .insert(state.mark());
+    // Octree
+    commands
+        .spawn()
+        .insert(OctreeNode::new(64., Vec3::new(0., 32., 0.)))
+        .insert(state.mark());
 }
 
 fn grab_cursor(mut windows: ResMut<Windows>) {
@@ -208,5 +214,303 @@ fn move_camera(
             to_move -= up;
         }
         transform.translation += to_move.clamp_length_max(1.0) * delta;
+    }
+}
+
+#[derive(Component)]
+pub struct Collides;
+
+#[derive(Component, Clone, Copy)]
+pub struct BoundingBox {
+    min: Vec3,
+    max: Vec3,
+}
+
+impl BoundingBox {
+    pub fn new(min: Vec3, max: Vec3) -> Self {
+        if min.cmpgt(max).any() {
+            panic!("min value of BoundingBox is bigger than max");
+        }
+        Self { min, max }
+    }
+
+    pub fn from_size(size: f32, offset: Vec3) -> Self {
+        Self {
+            min: offset - size * 0.5,
+            max: offset + size * 0.5,
+        }
+    }
+
+    pub fn length(&self) -> Vec3 {
+        self.max - self.min
+    }
+
+    pub fn x_length(&self) -> f32 {
+        self.max.x - self.min.x
+    }
+
+    pub fn y_length(&self) -> f32 {
+        self.max.y - self.min.y
+    }
+
+    pub fn z_length(&self) -> f32 {
+        self.max.z - self.min.z
+    }
+
+    pub fn center(&self) -> Vec3 {
+        (self.min + self.max) * 0.5
+    }
+
+    pub fn center_x(&self) -> f32 {
+        (self.min.x + self.max.x) * 0.5
+    }
+
+    pub fn center_y(&self) -> f32 {
+        (self.min.y + self.max.y) * 0.5
+    }
+
+    pub fn center_z(&self) -> f32 {
+        (self.min.z + self.max.z) * 0.5
+    }
+
+    pub fn octants(&self) -> Option<BVec3> {
+        let x_p = self.min.x > 0. && self.max.x > 0.;
+        let x_n = self.min.x < 0. && self.max.x < 0.;
+        let y_p = self.min.y > 0. && self.max.y > 0.;
+        let y_n = self.min.y < 0. && self.max.y < 0.;
+        let z_p = self.min.z > 0. && self.max.z > 0.;
+        let z_n = self.min.z < 0. && self.max.z < 0.;
+        if x_p ^ x_n && y_p ^ y_n && z_p ^ z_n {
+            Some(BVec3::new(x_p, y_p, z_p))
+        } else {
+            None
+        }
+    }
+
+    pub fn get_octant(&self, x: bool, y: bool, z: bool) -> Self {
+        let (min_x, max_x) = if x {
+            (self.center_x(), self.max.x)
+        } else {
+            (self.min.x, self.center_x())
+        };
+        let (min_y, max_y) = if y {
+            (self.center_y(), self.max.y)
+        } else {
+            (self.min.y, self.center_y())
+        };
+        let (min_z, max_z) = if z {
+            (self.center_z(), self.max.z)
+        } else {
+            (self.min.z, self.center_z())
+        };
+        Self::new(
+            Vec3::new(min_x, min_y, min_z),
+            Vec3::new(max_x, max_y, max_z),
+        )
+    }
+}
+
+impl Add<f32> for BoundingBox {
+    type Output = BoundingBox;
+
+    fn add(self, rhs: f32) -> Self::Output {
+        Self::Output {
+            min: self.min + rhs,
+            max: self.max + rhs,
+        }
+    }
+}
+
+impl Sub<f32> for BoundingBox {
+    type Output = BoundingBox;
+
+    fn sub(self, rhs: f32) -> Self::Output {
+        Self::Output {
+            min: self.min - rhs,
+            max: self.max - rhs,
+        }
+    }
+}
+
+impl Add<Vec3> for BoundingBox {
+    type Output = BoundingBox;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Self::Output {
+            min: self.min + rhs,
+            max: self.max + rhs,
+        }
+    }
+}
+
+impl Sub<Vec3> for BoundingBox {
+    type Output = BoundingBox;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Self::Output {
+            min: self.min - rhs,
+            max: self.max - rhs,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct OctreeEntity {
+    entity: Entity,
+    bound: BoundingBox,
+}
+
+impl OctreeEntity {
+    pub fn new(entity: Entity, bound: BoundingBox) -> Self {
+        Self { entity, bound }
+    }
+}
+
+impl Deref for OctreeEntity {
+    type Target = BoundingBox;
+
+    fn deref(&self) -> &Self::Target {
+        &self.bound
+    }
+}
+
+impl Eq for OctreeEntity {}
+
+impl PartialEq for OctreeEntity {
+    fn eq(&self, other: &Self) -> bool {
+        self.entity.eq(&other.entity)
+    }
+}
+
+impl PartialOrd for OctreeEntity {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.entity.partial_cmp(&other.entity)
+    }
+}
+
+impl Ord for OctreeEntity {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.entity.cmp(&other.entity)
+    }
+}
+
+#[derive(Component)]
+pub struct OctreeNode {
+    bound: BoundingBox,
+    entities: BTreeSet<OctreeEntity>,
+    leaves: BTreeMap<u32, OctreeNode>,
+}
+
+impl OctreeNode {
+    pub fn new(size: f32, offset: Vec3) -> OctreeNode {
+        Self::from_bound(BoundingBox::from_size(size, offset))
+    }
+
+    pub fn from_bound(bound: BoundingBox) -> Self {
+        OctreeNode {
+            bound,
+            entities: BTreeSet::new(),
+            leaves: BTreeMap::new(),
+        }
+    }
+
+    pub fn removal_system(
+        mut octree: Query<&mut OctreeNode>,
+        removals: RemovedComponents<Collides>,
+        query: Query<(&Transform, &BoundingBox), Added<Collides>>,
+    ) {
+        let mut octree = octree.single_mut();
+        for entity in removals.iter() {
+            let (transform, bound) = query.get(entity).expect("Ghost entity");
+            octree.remove(entity, *bound + transform.translation);
+        }
+    }
+
+    pub fn insertion_system(
+        mut octree: Query<&mut OctreeNode>,
+        query: Query<(Entity, &Transform, &BoundingBox), Added<Collides>>,
+    ) {
+        let mut octree = octree.single_mut();
+        for (entity, transform, bound) in query.iter() {
+            octree.insert(entity, *bound + transform.translation);
+        }
+    }
+
+    pub fn collision_system() {}
+
+    const fn octant_to_index(&self, x: bool, y: bool, z: bool) -> u32 {
+        const STEP_X: u32 = 4;
+        const STEP_Y: u32 = 2;
+        const STEP_Z: u32 = 1;
+        STEP_X * x as u32 + STEP_Y * y as u32 + STEP_Z * z as u32
+    }
+
+    pub fn insert(&mut self, entity: Entity, bound: BoundingBox) -> bool {
+        self.insert_inner(OctreeEntity::new(entity, bound))
+    }
+
+    fn insert_inner(&mut self, entity: OctreeEntity) -> bool {
+        // kinda threshold
+        if self.entities.len() >= 4 {
+            let center = self.bound.center();
+            let mut to_leaves =
+                Vec::<(OctreeEntity, bool, bool, bool)>::with_capacity(self.entities.len() + 1);
+
+            let mut ret = match (entity.bound - center).octants() {
+                Some(BVec3 { x, y, z }) => {
+                    to_leaves.push((entity, x, y, z));
+                    true
+                }
+                None => self.entities.insert(entity),
+            };
+
+            self.entities
+                .retain(|&entity| match (entity.bound - center).octants() {
+                    Some(BVec3 { x, y, z }) => {
+                        to_leaves.push((entity, x, y, z));
+                        false
+                    }
+                    None => true,
+                });
+
+            for (entity, x, y, z) in to_leaves {
+                let i = self.octant_to_index(x, y, z);
+                ret &= match self.leaves.get_mut(&i) {
+                    Some(leaf) => leaf.insert_inner(entity),
+                    None => {
+                        let mut new_leaf = Self::from_bound(self.bound.get_octant(x, y, z));
+                        new_leaf.insert_inner(entity);
+                        self.leaves.insert(i, new_leaf);
+                        true
+                    }
+                };
+            }
+            ret
+        } else {
+            self.entities.insert(entity)
+        }
+    }
+
+    pub fn remove(&mut self, entity: Entity, bound: BoundingBox) -> bool {
+        self.remove_inner(OctreeEntity::new(entity, bound))
+    }
+
+    fn remove_inner(&mut self, entity: OctreeEntity) -> bool {
+        match (entity.bound - self.bound.center()).octants() {
+            Some(BVec3 { x, y, z }) => {
+                let i = self.octant_to_index(x, y, z);
+                match self.leaves.get_mut(&i) {
+                    Some(leaf) => {
+                        let ret = leaf.remove_inner(entity);
+                        if ret && leaf.entities.is_empty() {
+                            self.leaves.remove(&i);
+                        }
+                        ret
+                    }
+                    None => self.entities.remove(&entity),
+                }
+            }
+            None => self.entities.remove(&entity),
+        }
     }
 }
